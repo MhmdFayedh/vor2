d80aba8e3c9ed1d2e2161faa85502b54bf92d266
MhmdFayedh <mhmdfayedh@gmail.com>
2024-05-26 01:49:07 +0300

    Add accounts with the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,52 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        //* Create Admin account
+        //* Admins account
         \App\Models\User::create([
-            'name' => 'محمد الفايض',
-            'email' => 'mo@ex.com',
-            'password' => 'Kikpwer123456$',
+            'name' => 'محمد الشمري',
+            'email' => 'am.alshammari@neogen.com',
+            'password' => 'MohammedFayedh010101',
             'role' => 'admin',
         ]);
+
+        \App\Models\User::create([
+            'name' => 'محمد الزبني',
+            'email' => 'am.alzubayni@neogen.com',
+            'password' => 'MohammedFayedh010101',
+            'role' => 'admin',
+        ]);
+
+
+        \App\Models\User::create([
+            'name' => 'مشاعل',
+            'email' => 'am.m@neogen.com',
+            'password' => 'Mashael010101',
+            'role' => 'admin',
+        ]);
+
+        //* Subervisor account
+        \App\Models\User::create([
+            'name' => 'محمد الشمري',
+            'email' => 'sm.alshammari@neogen.com',
+            'password' => 'MohammedFayedh010101',
+            'role' => 'supervisor',
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'محمد الزبني',
+            'email' => 'sm.alzubayni@neogen.com',
+            'password' => 'MohammedFayedh010101',
+            'role' => 'supervisor',
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'مشاعل',
+            'email' => 'sm.m@neogen.com',
+            'password' => 'Mashael010101',
+            'role' => 'supervisor',
+        ]);
+
+
 
         // Create Requests and user for each request
         \App\Models\Request::factory(50)->create();
